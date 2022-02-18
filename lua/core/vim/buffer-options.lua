4450a56fd38a8babedf41b6899cf23eb721aeac8
da-moon <contact@havi.dev>
2022-02-18 21:09:39 +0000

    feat(core/vim): `buffer` options.

---
+++ lua/core/vim/buffer-options.lua
@@ -0,0 +1,8 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+
+-- stylua: ignore start
+vim.bo.smartindent = true  --- Makes indenting smart
+vim.bo.shiftwidth = 2      --- Change a number of space characeters inserted for indentation
+vim.bo.expandtab = true
+-- stylua: ignore end
