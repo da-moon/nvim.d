f3973a47443545c8022d789140eee1336f1528ca
da-moon <contact@havi.dev>
2022-02-18 21:09:13 +0000

    feat(core/vim): `global` variables.

---
+++ lua/core/vim/global.lua
@@ -0,0 +1,26 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+
+-- stylua: ignore start
+vim.g.mapleader = " "       --- Map leader key
+vim.g.maplocalleader = ","  --- Map local leader key
+vim.g.hidden = true         --- Required to keep multiple buffers open multiple buffers
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_logipat = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+-- stylua: ignore end
