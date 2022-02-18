0783e20645b804c179b3019b6cb43e1814bf4f9e
da-moon <contact@havi.dev>
2022-02-18 21:11:06 +0000

    feat(core): module setup.

---
+++ lua/core/init.lua
@@ -0,0 +1,13 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+local utils = require("lib.utils")
+local modules = {
+   "core.vim",
+   "core.colors",
+}
+for _, module in ipairs(modules) do
+   local status, _ = pcall(require, module)
+   if not status then
+      print(module .. " not found!")
+   end
+end
