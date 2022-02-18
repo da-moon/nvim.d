14758f2b0d3380e8bfc8225563de443f921580cc
da-moon <contact@havi.dev>
2022-02-18 21:08:31 +0000

    feat(core/vim): `window` options.

---
+++ lua/core/vim/window-options.lua
@@ -0,0 +1,9 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+
+-- stylua: ignore start
+vim.wo.number = true          --- Shows current line number
+vim.wo.relativenumber = true  --- Enables relative number
+vim.wo.cursorline = true      --- Highlight of current line
+vim.wo.wrap = false           --- Display long lines as just one line. https://neovim.io/doc/user/options.html#'wrap'
+-- stylua: ignore end
