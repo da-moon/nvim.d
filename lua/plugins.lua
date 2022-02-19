ad2415b6513548c0181bcf3b4aa9076b3ba75e5f
da-moon <contact@havi.dev>
2022-02-19 01:09:33 +0000

    feat(plugins): added `nvim-telescope/telescope.nvim` to Packer plugins table.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,23 +25,35 @@ packer.startup(function(use)
    -- ╭──────────────────────────────────────────────────────────╮
    -- │                  neovim-lua-development                  │
    -- ╰──────────────────────────────────────────────────────────╯
-   use({ "wbthomason/packer.nvim"                                                 })
-   use({ "nvim-lua/plenary.nvim"        , opt = false                             })
-   use({ "nvim-lua/popup.nvim"          , opt = false                             })
-   use({ "MunifTanjim/nui.nvim"         , opt = false                             })
-   use({ "kyazdani40/nvim-web-devicons" , opt = false                             })
-   use({ "ray-x/guihua.lua"             , opt = false, run = "cd lua/fzy && make" })
-   -- stylua: ignore end
-   -- luacheck: max line length 120
+   use({ "wbthomason/packer.nvim"                                                                                     })
+   use({ "nvim-lua/plenary.nvim"        , opt =  false                                                                })
+   use({ "nvim-lua/popup.nvim"          , opt =  false                                                                })
+   use({ "MunifTanjim/nui.nvim"         , opt =  false                                                                })
+   use({ "kyazdani40/nvim-web-devicons" , opt =  false                                                                })
    -- ╭──────────────────────────────────────────────────────────╮
    -- │                       key bindings                       │
    -- ╰──────────────────────────────────────────────────────────╯
    use({
       "folke/which-key.nvim",
-      opt = false,
-      config = [[require("plugins.which-key").config()]],
+      opt      = false,
+      config   = [[require("plugins.which-key").config()]],
    })
+   -- ╭──────────────────────────────────────────────────────────╮
+   -- │                       fuzzy finder                       │
+   -- ╰──────────────────────────────────────────────────────────╯
+   use({ "ray-x/guihua.lua"             , opt =  false, run = "cd lua/fzy && make"                                    })
+   use({ "junegunn/fzf"                 , opt =  false, run = "./install--bin"                                        })
+   use({ "ibhagwan/fzf-lua"             , opt =  false, requires = { "kyazdani42/nvim-web-devicons", "junegunn/fzf" } })
+   use({
+      "nvim-telescope/telescope.nvim",
+      opt      = false,
+      requires = { "nvim-lua/plenary.nvim", "ibhagwan/fzf-lua" },
+      after    = { "plenary.nvim", "fzf-lua"                   },
+      config   = [[require("plugins.telescope").config()]],
+   })
+   -- stylua: ignore end
    if should_sync then
       packer.sync()
    end
 end)
+-- luacheck: max line length 120
