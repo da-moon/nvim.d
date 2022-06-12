e426a87fc069e4a8e7066a72c201b44847c03cdd
da-moon <contact@havi.dev>
2022-06-12 14:35:44 -0400

    feat(lua): `core` setup

--- lua/core.lua
+++ lua/core.lua
@@ -1,6 +1,5 @@
 -- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
 -- code: language=lua insertSpaces=true tabSize=3
-local utils = require("lib.utils")
 local modules = {
    "core.vim",
    "core.colors",
