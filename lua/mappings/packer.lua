c17924b3be822bc8e6d1881acd118634b2650d4e
da-moon <contact@havi.dev>
2022-06-12 14:35:45 -0400

    feat(lua/mappings): `packer` setup

--- lua/mappings/packer.lua
+++ lua/mappings/packer.lua
@@ -3,9 +3,12 @@
 -- ──────────────────────────────────────────────────────────────────────
 -- selene: allow(unused_variable)
 -- luacheck: no unused args
-return function(register, plugin_loader)
+return function(register)
    -- selene: deny(unused_variable)
    -- luacheck: unused args
+   if not register then
+      return
+   end
    register({
       name = "+Packer",
       ["C"] = { [[<cmd>PackerClean<cr>]], "Clean Plugins" },
