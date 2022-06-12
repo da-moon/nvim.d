e48703571368cf9a3c3092b931ddb19a63f50c5d
da-moon <contact@havi.dev>
2022-06-12 14:35:45 -0400

    feat(lua): `mappings` setup

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,31 +2,56 @@
 -- code: language=lua insertSpaces=true tabSize=3
 -- ──────────────────────────────────────────────────────────────────────
 -- [ TODO ] :
--- - [ ] redfine keybindings based on plugin so that
+-- - [ ] redefine keybindings based on plugin so that
 -- it can be used in Packer's load conditions.
 -- ──────────────────────────────────────────────────────────────────────
-local packer_status, packer = pcall(require, "packer")
-if not packer_status then
-   return print("mappings: failed to load plugin: packer")
-end
-local pluginman_status, pluginman = pcall(require, "lib.plugin-manager")
-if not pluginman_status then
-   return print("mappings: failed to load plugin:lib.plugin-manager ")
-end
+local pluginman = require("lib.plugin-manager")
+local logger = require("lib.logger")()
+local msg = ""
+-- ──────────────────────────────────────────────────────────────────────
 local wk = pluginman:load_plugin("which-key.nvim", "which-key")
 if not wk then
-   return print("Failed to load plugin: which-key")
+   msg = string.format("Failed to load plugin: which-key")
+   -- stylua: ignore start
+   if logger then return logger:warn(msg) else return end
+   -- stylua: ignore end
 end
 local modules = {
    "mappings.core",
+   "mappings.buffer",
    "mappings.packer",
    "mappings.telescope",
+   "mappings.files",
+   "mappings.terminal",
+   "mappings.applications",
+   "mappings.lightspeed-nvim",
+   "mappings.surround-nvim",
+   "mappings.trouble-nvim",
+   "mappings.comment-nvim",
+   "mappings.comment-box",
+   "mappings.todo-comments-nvim",
+   "mappings.zen-mode-nvim",
+   "mappings.nvim-spectre",
+   "mappings.substitute-nvim",
+   "mappings.registers-nvim",
+   "mappings.help",
+   "mappings.luasnip",
+   "mappings.lsp",
+   "mappings.local-leader",
+   "mappings.vim-easy-align",
 }
 for _, module in ipairs(modules) do
    local status, map = pcall(require, module)
-   if not status then
-      print(module .. " not found!")
+   if not map then
+      msg = module .. " not found!"
+      -- stylua: ignore start
+      if logger then logger:warn(msg)  end
+      -- stylua: ignore end
    else
-      map(wk.register, packer.loader)
+      msg = string.format("loading mappings from [ %s ]", module)
+      -- stylua: ignore start
+      if logger then logger:trace(msg) end
+      -- stylua: ignore end
+      map(wk.register)
    end
 end
