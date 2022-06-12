25c024e146a97d75fa28fc6ad0cc5a3c101ad2cc
da-moon <contact@havi.dev>
2022-06-12 14:44:28 -0400

    feat(after/plugin): `which-key` setup

---
+++ after/plugin/which-key.lua
@@ -0,0 +1,4 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+require("mappings")
+
