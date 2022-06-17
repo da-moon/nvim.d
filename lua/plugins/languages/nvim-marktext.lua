f3a0f69caeceb008fd30f707e401b51596c500d6
da-moon <contact@havi.dev>
2022-06-17 13:40:28 -0400

    feat(plugins/languages): `nvim-marktext` setup

---
+++ lua/plugins/languages/nvim-marktext.lua
@@ -0,0 +1,14 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+-- ────────────────────────────────────────────────────────────
+local pluginman = require("lib.plugin-manager")
+local logger = require("lib.logger")()
+local msg = ""
+-- ────────────────────────────────────────────────────────────
+local M = {}
+function M.cond()
+   return vim.fn.executable("marktext") > 0
+end
+function M.setup() end
+function M.config() end
+return M
