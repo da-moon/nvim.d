456b0e393aaefe13f27a9221f65244a257b05ecf
da-moon <contact@havi.dev>
2022-06-17 13:40:27 -0400

    feat(plugins/core/lsp): `lspkind-nvim` setup

---
+++ lua/plugins/core/lsp/lspkind-nvim.lua
@@ -0,0 +1,32 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+-- ──────────────────────────────────────────────────────────────────────
+local pluginman = require("lib.plugin-manager")
+-- ──────────────────────────────────────────────────────────────────────
+local M = {}
+function M.cond()
+   return true
+end
+function M.setup() end
+function M.config()
+   local module_name = "lspkind"
+   local plugin_name = "lspkind-nvim"
+   local plug = pluginman:load_plugin(plugin_name, module_name)
+   assert(
+      plug ~= nil,
+      string.format(
+         "module < %s > from plugin <%s> could not get loaded  [ %s ]",
+         module_name,
+         plugin_name,
+         debug.getinfo(1, "S").source:sub(2)
+      )
+   )
+   -- stylua: ignore end
+   plug.init({
+      -- with_text = true,
+      mode = "symbol_text",
+      preset = "default",
+      symbol_map = { Field = "" },
+   })
+end
+return M
