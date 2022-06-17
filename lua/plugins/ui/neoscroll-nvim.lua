f467a9781cab8a3f37637118e8e24e99b4938b1f
da-moon <contact@havi.dev>
2022-06-17 13:40:28 -0400

    feat(plugins/ui): `neoscroll-nvim` setup

---
+++ lua/plugins/ui/neoscroll-nvim.lua
@@ -0,0 +1,49 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+-- ────────────────────────────────────────────────────────────
+local pluginman = require("lib.plugin-manager")
+local logger = require("lib.logger")()
+local msg = ""
+-- ────────────────────────────────────────────────────────────
+local M = {}
+function M.config()
+   local to_require_map = {
+      ["neoscroll.nvim"] = { ["neoscroll"] = {} },
+   }
+   for plugin_name, modules in pairs(to_require_map) do
+      for module_name, _ in pairs(modules) do
+         local plug = pluginman:load_plugin(plugin_name, module_name)
+         if not plug then
+            msg = string.format("module < %s > from plugin <%s> could not get loaded", module_name, plugin_name)
+               -- stylua: ignore start
+               if logger then logger:warn(msg)  end
+            -- stylua: ignore end
+         end
+         to_require_map[plugin_name][module_name] = plug
+      end
+   end
+   local neoscroll = to_require_map["neoscroll.nvim"]["neoscroll"]
+   assert(
+      neoscroll ~= nil,
+      string.format(
+         "module < %s > from plugin <%s> could not get loaded  [ %s ]",
+         "neoscroll",
+         "neoscroll.nvim",
+         debug.getinfo(1, "S").source:sub(2)
+      )
+   )
+   neoscroll.setup({
+      -- All these keys will be mapped to their corresponding default scrolling animation
+      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+      hide_cursor = true, -- Hide cursor while scrolling
+      stop_eof = true, -- Stop at <EOF> when scrolling downwards
+      use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      easing_function = nil, -- Default easing function
+      pre_hook = nil, -- Function to run before the scrolling animation starts
+      post_hook = nil, -- Function to run after the scrolling animation ends
+      performance_mode = false, -- Disable "Performance Mode" on all buffers.
+   })
+end
+return M
