43b8c8e1bf4e5a407c5378880b975c7b1e84540b
da-moon <contact@havi.dev>
2022-06-17 13:40:27 -0400

    feat(plugins/core/treesitter): `spellsitter-nvim` setup

---
+++ lua/plugins/core/treesitter/spellsitter-nvim.lua
@@ -0,0 +1,25 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+-- ──────────────────────────────────────────────────────────────────────
+local pluginman = require("lib.plugin-manager")
+-- ──────────────────────────────────────────────────────────────────────
+local M = {}
+function M.config()
+   local module_name = "spellsitter"
+   local plugin_name = "spellsitter.nvim"
+   local plug = pluginman:load_plugin(plugin_name, module_name)
+   assert(
+      plug ~= nil,
+      string.format(
+         "module < %s > from plugin <%s> could not get loaded  [ %s ]",
+         module_name,
+         plugin_name,
+         debug.getinfo(1, "S").source:sub(2)
+      )
+   )
+   plug.setup({
+      hl = "SpellBad",
+      captures = { "comment" }, -- set to {} to spellcheck everything
+   })
+end
+return M
