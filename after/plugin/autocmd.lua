add640396273c8dcc4745fa9a54bd123a00fa9eb
da-moon <contact@havi.dev>
2022-06-17 16:24:40 -0400

    fix(after/plugin): do not split screen when in `NvimTree` buffer

--- after/plugin/autocmd.lua
+++ after/plugin/autocmd.lua
@@ -15,3 +15,8 @@ vim.cmd('autocmd FileType dashboard lua require("autocmd.dashboard.on_enter")()'
 -- Add cursorline and diasable it in terminal
 -- vim.cmd 'autocmd WinEnter,BufEnter * if &ft is "toggleterm" | set nocursorline | else | set cursorline | endif'
 vim.cmd('autocmd! TermOpen,TermEnter  term://* lua require("autocmd.terminal.on_enter")()')
+-- ╭──────────────────────────────────────────────────────────╮
+-- │                  nvim-tree keybindings                   │
+-- ╰──────────────────────────────────────────────────────────╯
+-- disable certain keybindings when cursor is on nvim-tree
+vim.cmd('autocmd FileType NvimTree lua require("autocmd.nvim-tree.on_enter")()')
