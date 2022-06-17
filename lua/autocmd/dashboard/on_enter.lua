9804fb772c753de9596b04ef0baac86c1452ae80
da-moon <contact@havi.dev>
2022-06-17 13:31:43 -0400

    feat(autocmd/dashboard): initial commit

---
+++ lua/autocmd/dashboard/on_enter.lua
@@ -0,0 +1,10 @@
+-- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
+-- code: language=lua insertSpaces=true tabSize=3
+-- ──────────────────────────────────────────────────────────────────────
+return function()
+   local opts = { noremap = true }
+   vim.api.nvim_buf_set_keymap(0, "n", "<C-Up>", "<C-w>k", opts)
+   vim.api.nvim_buf_set_keymap(0, "n", "<C-Down>", "<C-w>j", opts)
+   vim.api.nvim_buf_set_keymap(0, "n", "<C-Left>", "<C-w>h", opts)
+   vim.api.nvim_buf_set_keymap(0, "n", "<C-Right>", "<C-w>l", opts)
+end
