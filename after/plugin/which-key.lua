b7c8f3ef306042309aac90496d1fca317bd3e4a8
da-moon <contact@havi.dev>
2022-06-17 13:30:45 -0400

    fix(after/plugin): `which-key` mappings at initial bootstrap error

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -1,4 +1,3 @@
 -- vim: filetype=lua syntax=lua softtabstop=3 tabstop=3 shiftwidth=3 fileencoding=utf-8 smartindent autoindent expandtab
 -- code: language=lua insertSpaces=true tabSize=3
-require("mappings")
-
+pcall(require, "mappings")
