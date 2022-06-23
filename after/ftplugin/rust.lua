cdf34c5d206742c30f914105795a4430464af131
da-moon <contact@havi.dev>
2022-06-23 10:48:14 -0400

    feat(after/ftplugin/rust): keybinding for hover action

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -11,6 +11,8 @@ local to_require_map = {
    ["rust-tools.nvim"] = {
       ["rust-tools"] = {},
       ["rust-tools.inlay_hints"] = {},
+      ["rust-tools.hover_actions"] = {},
+      ["rust-tools.hover_range"] = {},
    },
 }
 for plugin_name, modules in pairs(to_require_map) do
@@ -45,16 +47,60 @@ if not wk then
    if logger then return logger:warn(msg) else return end
    -- stylua: ignore end
 end
+local inlay_hints = to_require_map["rust-tools.nvim"]["rust-tools.inlay_hints"]
+if not inlay_hints then
+   msg = string.format("could not load 'rust-tools.inlay_hints'")
+   -- stylua: ignore start
+   if logger then return logger:warn(msg) end
+   -- stylua: ignore end
+end
+local hover_actions = to_require_map["rust-tools.nvim"]["rust-tools.hover_actions"]
+if not hover_actions then
+   msg = string.format("could not load 'rust-tools.hover_actions'")
+   -- stylua: ignore start
+   if logger then return logger:warn(msg) end
+   -- stylua: ignore end
+end
 wk.register({
    ["h"] = {
       function()
-         to_require_map["rust-tools.nvim"]["rust-tools.inlay_hints"].toggle_inlay_hints()
+        if inlay_hints then
+          inlay_hints.toggle_inlay_hints()
+        end
       end,
       "Toggle inlay hints",
    },
+   ["<SPACE>"] = {
+    function()
+     if hover_actions then
+       hover_actions.hover_actions()
+       vim.lsp.buf.hover()
+     end
+    end,
+    "Toggle hover actions",
+ },
 }, {
    mode = "n",
    prefix = "<LocalLeader>",
    buffer = vim.api.nvim_get_current_buf(),
    noremap = true,
+})
+wk.register({
+  ["<C-SPACE>"] = {
+     function()
+      if hover_actions then
+        -- https://www.reddit.com/r/neovim/comments/uk3xmq/comment/i7n6wr6/?utm_source=share&utm_medium=web2x&context=3
+        -- https://vi.stackexchange.com/questions/31422/how-to-switch-mode-from-visual-visual-line-mode-to-normal-mode-in-lua
+        -- https://vi.stackexchange.com/questions/31422/how-to-switch-mode-from-visual-visual-line-mode-to-normal-mode-in-lua
+        vim.cmd('stopinsert')
+        hover_actions.hover_actions()
+        vim.lsp.buf.hover()
+      end
+     end,
+     "Toggle hover actions",
+  },
+}, {
+  mode = "i",
+  buffer = vim.api.nvim_get_current_buf(),
+  noremap = true,
 })
