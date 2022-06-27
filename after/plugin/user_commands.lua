ea64d3f93f1cf3c72774ce8d42922d30a8922cbd
da-moon <contact@havi.dev>
2022-06-27 12:46:21 -0400

    feat(after/plugin/user_commands): collection of utility commands

---
+++ after/plugin/user_commands.lua
@@ -0,0 +1,33 @@
+-- NeatFoldText
+vim.api.nvim_create_user_command(
+	'SpacesToTabs',
+	function(tbl)
+		vim.opt.expandtab = false
+		local previous_tabstop = vim.bo.tabstop
+		vim.opt.tabstop = tonumber(tbl.args)
+		vim.api.nvim_command 'retab!'
+		vim.opt.tabstop = previous_tabstop
+	end,
+	{force = true, nargs = 1}
+)
+
+vim.api.nvim_create_user_command(
+	'TabsToSpaces',
+	function(tbl)
+		vim.opt.expandtab = true
+		local previous_tabstop = vim.bo.tabstop
+		vim.opt.tabstop = tonumber(tbl.args)
+		vim.api.nvim_command 'retab'
+		vim.opt.tabstop = previous_tabstop
+	end,
+	{force = true, nargs = 1}
+)
+
+-- Vim LSP stuff
+vim.api.nvim_create_user_command(
+	'LspOrganizeImports',
+	'lua vim.lsp.buf.code_action{source = {organizeImports = true}}',
+	{force = true}
+)
+
+
